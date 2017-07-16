88cd5810172fa9e85a0ce691b86d5e07413a8e23
taro <3128208075@qq.com>
2017-07-16 13:16:36 +0800

    fix up

--- src/l10n/zh.js
+++ src/l10n/zh.js
@@ -3,7 +3,7 @@ var flatpickr = flatpickr || {l10ns: {}};
 flatpickr.l10ns.zh = {};
 
 flatpickr.l10ns.zh.weekdays = {
-	shorthand: ["周日", "周一", "周二", "周三", "周四", "周五", "周六"],
+	shorthand: ["日", "一", "二", "三", "四", "五", "六"],
 	longhand: ["星期日", "星期一", "星期二", "星期三", "星期四", "星期五", "星期六"]
 };
 
